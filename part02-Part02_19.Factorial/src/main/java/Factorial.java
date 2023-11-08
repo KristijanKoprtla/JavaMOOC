c10fbb041d9f4dc71c78b5789bbc0b2cbc45355f
BombMarley <kile146@gmail.com>
2023-11-08 18:37:04 +0100

    test commit, finish factorial exercise

--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -5,6 +5,14 @@ public class Factorial {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Give a number:");
+        int num = Integer.valueOf(scanner.nextLine());
+        int factorialNum = 1;
+        
+        for(int i = 1; i <= num; i++){
+            factorialNum *= i;
+        }
+        
+        System.out.println("Factorial: " + factorialNum);
     }
 }
